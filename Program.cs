af437e23a3fa596875c478ec3f57373a9ce1f182
Unai Almiñana <unai.alminana.7e9@itb.cat>
2025-10-30 13:05:09 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.ComponentModel;
 
-public class Program
+class Program
 {
     static void Main()
     {
+        //MAIN MENU
         const string MenuTitle = "===== MAIN MENU - CODEQUEST =====";
         const string MenuOption1 = "1. Train your wizard";
         const string MenuOption2 = "2. Check the dungeon";
@@ -15,6 +17,28 @@ public class Program
         int op = 0;
         bool validInput;
 
+        //CHAPTER ONE
+        const string ChOneTitle = "===== CHAPTER ONE =====";
+        const string ChOneBegin = "Welcome to the arcane academy! Let's start training your magic power in the next 5 days.";
+        const string ChOneName = "Please choose your wizard name (max of 15 characters):";
+        const string PowerNoob = "You are weak, you think that spoons are the same that magic wands...";
+        const string PowerPro = "Almost there! At least you can conjure little magic breezes...";
+        const string PowerHacker = "Yeah! your power is pretty impressive! you can invoke all sorts of dragons and conjure different spells.";
+        const string PowerGod = "Congratulations! your power is worth of an arcane master! Your knowldedge is unlimited.";
+        const string NameError = "Invalid name, please input a name with at least one character and shorter than 15 characters.";
+
+        const string LevelNoob = ", The Bread Head, That's your level.";
+        const string LevelPro = " Peppermint, That's your level.";
+        const string LevelHacker = ", The red magician, That's your level.";
+        const string LevelGod = ", Jujutsu of Oblivion, That's your level.";
+
+        string? name;
+        var randomNum = new Random();
+        int totalPow = 0;
+        int actualPow = 0;
+        //CHAPTER TWO
+        //const string ChTwoTitle = "===== CHAPTER TWO =====";
+
         do
         {
             Console.WriteLine(MenuTitle);
@@ -24,8 +48,6 @@ public class Program
             Console.WriteLine(MenuOptionExit);
             Console.Write(MenuPrompt);
 
-            validInput = true;
-
             try
             {
                 op = Convert.ToInt32(Console.ReadLine());
@@ -33,22 +55,86 @@ public class Program
             }
             catch (FormatException)
             {
-                Console.WriteLine(InputErrorMessage);
                 validInput = false;
             }
             catch (Exception)
             {
-                Console.WriteLine(InputErrorMessage);
                 validInput = false;
+            }
+
+            if (op > 3 || op < 0)
+            {
+                validInput = false;
+            }
+            else
+            {
+                validInput = true;
             }
 
             if (validInput)
             {
                 Console.WriteLine(op);
             }
+            else 
+            {
+                Console.WriteLine(InputErrorMessage);
+            }
 
+            
+
+            if (op == 1) 
+            {
+                //CHAPTER 1:
+                Console.WriteLine(ChOneTitle);
+                Console.WriteLine(ChOneBegin);
+                Console.WriteLine(ChOneName);
+                name = Console.ReadLine();
+
+                if (String.IsNullOrWhiteSpace(name) || name.Length > 15) 
+                {
+                    while (String.IsNullOrWhiteSpace(name) || name.Length > 15)
+                    {
+                        Console.WriteLine(NameError);
+                        name = Console.ReadLine();
+                    }
+                }
+
+                for (int i = 1; i <= 5; i++)
+                {
+                    actualPow = randomNum.Next(1, 10);
+                    Console.WriteLine("--DAY " + i + ": you meditated 10 hours, you gained " + actualPow + " power points!");
+                    totalPow += actualPow;
+                    Console.WriteLine("Actual power: " + totalPow + " press ENTER to continue");
+                    Console.ReadLine();
+                }
+
+                switch (totalPow) 
+                {
+                    case <= 20:
+                        Console.WriteLine(PowerNoob);
+                        Console.WriteLine(name + LevelNoob);
+                        break;
+                    case <= 30:
+                        Console.WriteLine(PowerPro);
+                        Console.WriteLine(name + LevelPro);
+                        break;
+                    case <= 40:
+                        Console.WriteLine(PowerHacker);
+                        Console.WriteLine(name + LevelHacker);
+                        break;
+                    case <= 47:
+                        Console.WriteLine(PowerGod);
+                        Console.WriteLine(name + LevelGod);
+                        break;
+                    default:
+                        Console.WriteLine(PowerGod);
+                        Console.WriteLine(name + LevelGod);
+                        break;
+
+                }
+
+            }
 
         } while (op != 0);
     }
-
 }
